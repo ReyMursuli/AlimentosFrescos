bdd3fa8a4f5deced71ca752ba903716af113c5e0
ReyMursuli <reinaldo69.mursuli@gmail.com>
2025-02-02 00:42:56 +0100

    creados los modelos de la base de datos

--- alimentosFrescos/models.py
+++ alimentosFrescos/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    roll=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=200)
+    def __str__(self)
+         return f"{self.nombre} - {self.roll} ({self.telefono} -{self.direccion})"
+
+
+class Proveedor(Usuario):
+    class Meta:
+        verbose_name = 'Proveedor'
+        verbose_name_plural = 'Proveedores'
+
+class Administrador(Usuario):
+    class Meta:
+        verbose_name = 'Administrador'
+        verbose_name_plural = 'Administradores'
+
+class Cliente(Usuario):
+    tipo = models.CharField(max_length=100)
+    ci = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.tipo}-{self.ci}"
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
+class Pedido(models.Model): #los pedidos son las coompras realizadas por un cliente 
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='pedidos')
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    estado = models.CharField(max_length=50)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido de {self.cliente.nombre} - {self.estado}"
+
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    unidad_medida = models.CharField(max_length=50)
+    precio_costo = models.DecimalField(max_digits=10, decimal_places=2)
+    pedidos = models.ManyToManyField(Pedido, related_name='productos')
+
+    def __str__(self):
+        return f"{self.nombre} - {self.tipo}"
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+
+class Compra(models.Model):  #Una compra es lo que realiza alimentos frescos para la venta 
+    provedor = models.ForeingnKey(Provedor,on_delete=models.CASCADE,related_name="compras")
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE,RELATED_name="compras")
+    fecha = models.DateField()
+    cant = models.IntegerField()
+    precio = models.DecimalField()
+
+    def __str__(self)
+       return f"{self.provedor.nombre} - {self.producto.nombre} - {self.fecha} - {self.cant} - {self.precio}"
+
+    class Meta:
+        verbose_name = "Compra"
+        verbose_name_plural = "Compras"
+
+        
